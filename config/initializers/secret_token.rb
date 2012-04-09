528fde9b04960d5aba952ae996ea6ea3d7bf5841
Filipe A Moreira <famoreira@gmail.com>
2012-04-09 11:15:56 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PezcaloSpree10::Application.config.secret_token = '9d35fae542a7b7cc02e3edd60589d5e76fa80563e22f7adb139a7e94f32775ea4c45e14ee2780a2c0ff1f525f4628500207066fa1bcfa4e24429f530da8e06cc'
